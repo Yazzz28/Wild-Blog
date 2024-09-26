cf25bbc6e7141d80495f5806fe799be86cc506e5
Yacine MENNAA <yacinemennaa@gmail.com>
2024-09-26 10:41:24 +0200

    refresh quete3 pour Elise

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -11,34 +11,17 @@ import { Article } from '../models/article.model';
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent {
-  articles: Article[] = [
-    {
-      title: 'Titre de l\'article 1',
-      author: 'John Doe',
-      content: 'Voici le contenu du premier article.',
-      image: 'https://via.placeholder.com/350x150',
-      isPublished: true,
-      comment: ''
-    },
-    {
-      title: 'Titre de l\'article 2',
-      author: 'Jane Smith',
-      content: 'Voici le contenu du deuxième article.',
-      image: 'https://via.placeholder.com/350x150',
-      isPublished: false,
-      comment: ''
-    },
-    {
-      title: 'Titre de l\'article 3',
-      author: 'Alice Johnson',
-      content: 'Voici le contenu du troisième article.',
-      image: 'https://via.placeholder.com/350x150',
-      isPublished: true,
-      comment: ''
-    }
-  ];
+  article: Article = {
+    title: 'Titre de l\'article',
+    author: 'John Doe',
+    content: 'Voici le contenu de l\'article.',
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: '',
+    likes: 0
+  };
 
-  togglePublication(index: number): void {
-    this.articles[index].isPublished = !this.articles[index].isPublished;
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
   }
 }
--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -5,4 +5,5 @@ export interface Article {
   image: string;
   isPublished: boolean;
   comment: string;
+  likes: number;
 }
